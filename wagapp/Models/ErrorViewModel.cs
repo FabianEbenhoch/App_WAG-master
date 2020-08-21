40f3dff0d830037b02e113c59fac0dffafbf0db2
Fabian EBENHOCH <fabian.ebenhoch@hak-bregenz.at>
2020-08-21 17:26:33 +0200

    Projektdateien hinzufügen.

---
+++ wagapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wagapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
